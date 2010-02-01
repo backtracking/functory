e8c46a41faff35a7340e370062cbeba5189f85c3
filliatr <>
2010-02-01 23:12:37 +0000

    *** empty log message ***

--- master.ml
+++ master.ml
@@ -1,5 +1,5 @@
 
-(** The master implementation *)
+(** The generic master implementation *)
 
 (** The master context is a notion of workers, tasks and jobs *)
 module type CONTEXT = sig
@@ -17,15 +17,26 @@ module type CONTEXT = sig
         a time. *)
 
   val wait : unit -> Worker.t list
+    (** When the master is idle, it waits for worker completions using this
+	function. This function must return only workers for which a task was
+	assigned and is now completed. *)
 
   val extract : 'a job -> 'a task list
+    (** Whenever a job is completed, it is passed to this function, which may
+	produce new tasks in return. *)
 end
 
+(** Given a context, we provide a master implementation *)
 module Make(C : CONTEXT) : sig 
 
   val run : C.Worker.t list -> 'a C.task list -> unit
+    (** Runs the master with the given list of workers and tasks.
+	It returns only when there are not more pending tasks. *)
 
 end = struct
+
+  let run wl tl = 
+    assert false (* TODO *)
 
 end
 
