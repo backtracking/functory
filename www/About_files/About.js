91cca2423d4a90b713858bca278fbd7b2a48e107
filliatr <>
2010-05-03 13:58:41 +0000

    *** empty log message ***

---
+++ www/About_files/About.js
@@ -0,0 +1,7 @@
+// Created by iWeb 2.0.4 local-build-20100429
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('About_files/AboutMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');performPostEffectsFixups()}
